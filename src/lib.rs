668397ba702fb9807fb8a51adbae4279376c274b
Elliot Sayes <elliot@sayes.cloud>
2025-01-17 16:25:18 +0800

    Fix features

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
-pub use bevy;
 pub use reality_core;
+pub use bevy;
 
-#[cfg(feature = "client")]
+#[cfg(any(feature = "ao_client", feature = "web_client"))]
 pub use reality_player_interface;
 
-#[cfg(all(feature = "ao", feature = "server"))]
+#[cfg(feature = "ao_server")]
 pub use reality_ao_server;
+
+#[cfg(feature = "ao_client")]
+pub use reality_ao_client;
+
+#[cfg(feature = "web_client")]
+pub use reality_web_client;
